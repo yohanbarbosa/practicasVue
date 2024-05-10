7c906cee59b73d319d00388d3fe25238c6c81d99
Angelo Acosta Manzano <aaacostam@ufpso.edu.co>
2024-05-10 17:58:05 +0200

    practice vue

---
+++ src/components/vue/Directives.vue
@@ -0,0 +1,44 @@
+<script setup>
+import {ref} from 'vue';
+const checked = ref(true);
+
+const users = ["ford","jeep","honda", "subaru","aston martin"];
+
+</script>
+<!-- directivas: propiedades de un elemento -->
+
+<template>
+
+<div class="flex">
+    <div class="mx-auto">
+        <ul>
+            <li v-for="(user,i) in users":key="i++" >{{i}} {{user}}</li>
+        </ul>
+    </div>
+</div>
+
+
+<!-- 
+<div class="space-y-5">
+    <input type="checkbox" id="check" v-model="checked" class="mr-3" ><label for="check">aceptar</label>
+<div 
+class="bg-blue-500 text-white rounded px-2 w-44 mx-auto text-center py-5"
+:class="{'bg-green-500  font-semibold':checked }"
+
+>{{checked}}
+</div>
+</div>
+
+
+<div v-show="checked" >
+    mensaje para ocultar
+</div>
+
+<div v-if="checked" >
+    mensaje para ocultar
+</div>
+
+<div v-else >
+    no se cumplio la condicion del if
+</div> -->
+</template> 
---
+++ src/components/vue/TODO.vue
@@ -0,0 +1,31 @@
+<script setup>
+    import {ref} from 'vue';
+    const todo = ref('');
+    const list = ref([]);
+
+    function add(){
+        list.value.unshift(todo.value);
+        console.log(list);
+        todo.value = "";
+    }
+
+</script>
+
+<template>
+    <div class="flex items-center justify-center w-full h-screen">
+
+        <div class=" py-6 px-5 rounded shadow bg-slate-200 w-96">
+            <div class="">
+                <input v-model="todo" type="text" class="w-4/5  outline-none rounded px-2 py-1" >
+                <button @click="add"
+                 class="w-1/5 bg-green-600 text-white text-center py-1">+</button>
+
+            </div>
+            <div class="mt-3">
+                <div v-for="(todo,i) in list" :key="i++"
+                 class=" mt-1 bg-orange-300 rounded px-2"
+                 > {{todo}}</div>
+            </div>
+        </div>
+    </div>
+    </template>

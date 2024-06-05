15fc51ffaf2f20a747485f9057204936535256f4
yohanbarbosa <yohanbar30@gmail.com>
2024-06-05 11:24:50 -0500

    new component of vue

--- src/components/vue/Blog.vue
+++ src/components/vue/Blog.vue
@@ -1,4 +1,6 @@
+
 <script setup>
+
 import { ref } from 'vue';
 
 const notas = ["Nota1", "Nota2", "Nota3", "Nota4", "Nota5", "Nota6"];
@@ -15,13 +17,29 @@ function showActivar(dato) {
 </script>
 
 <template>
+
     <div class="w-full h-screen bg-slate-200 items-center flex">
         <div class="mx-auto w-2/4 border grid  gap-3 p-5 relative" 
         :class="estado ? 'grid-rows-6': 'grid-cols-3'"
         >
-            <div class="flex space-x-8 w-6 h-6 absolute top-[-50px] right-6">
-                <div class="cursor-pointer" @click="showActivar(1)">+</div>
-                <div class="cursor-pointer" @click="showActivar(0)">-</div>
+            <div class="flex space-x-2 w-24 h-24 absolute top-[-50px] right-6">
+                <div class=" grid grid-cols-2 cursor-pointer border border-black w-10 h-10 p-[2px]" 
+                :class="{'bg-[#caddd7]':!estado}"
+                @click="showActivar(1)">
+                    <span class="w-full h-full border border-black" ></span>
+                    <span class="w-full h-full border border-black" ></span>
+                    <span class="w-full h-full border border-black" ></span>
+                    <span class="w-full h-full border border-black" ></span>
+                </div>
+                <div class="cursor-pointer grid grid-rows-4 w-10 h-10 p-[2px] border border-black "
+                :class="{'bg-[#caddd7]':estado}"
+                @click="showActivar(0)">
+                    <span class="w-full h-full border border-black" ></span>
+                    <span class="w-full h-full border border-black" ></span>
+                    <span class="w-full h-full border border-black" ></span>
+                    <span class="w-full h-full border border-black" ></span>
+                </div>
+             
             </div>
             <div v-for="(nota, i) in notas" :key="i" class="bg-[#caddd7] w-full p-1 h-52" 
             :class="{'flex h-auto ':estado}"
---
+++ src/components/vue/Microblogging.vue
@@ -0,0 +1,48 @@
+<script setup>
+
+import { ref } from 'vue';
+
+const texto = ref('');
+const contador = ref(0);
+
+function medidor() {
+contador.value = texto.value.trim().split(/\s+/).length;
+console.log(contador.value);
+}
+
+</script>
+
+<template>
+
+    <div class="w-full h-screen flex items-center">
+        <video autoplay loop muted playsinline class="absolute inset-0 w-full h-full object-cover z-[-1]">
+      <source src="https://cdn.pixabay.com/video/2019/10/11/27770-365891067_large.mp4" type="video/mp4">
+      Tu navegador no soporta el video.
+    </video>
+
+        <div class="mx-auto w-2/4  text-center  flex-col">
+            <div class=" mx-auto w-2/5 justify-center flex items-end space-x-3">
+
+                <div class="bg-red-600 w-8 h-[40px]" v-if="contador >= 1 "></div>
+                <div class="bg-slate-400 w-8 h-[40px]" v-else></div>
+                <div class="bg-orange-600 w-8 h-[70px]" v-if="contador >= 3 "></div>
+                <div class="bg-slate-400 w-8 h-[70px]" v-else></div>
+                <div class="bg-yellow-600 w-8 h-[100px]" v-if="contador >= 5 "></div>
+                <div class="bg-slate-400 w-8 h-[100px]" v-else></div>
+                <div class="bg-green-600 w-8 h-[140px]" v-if="contador >= 7"></div>
+                <div class="bg-slate-400 w-8 h-[140px]" v-else></div>
+
+            </div>
+            <div class="">
+                <input v-model="texto" class=" px-2 bg-white mt-10 h-11 rounded-lg border border-black " type="text">
+                <button  @click="medidor()"
+                class="bg-slate-300 ml-6 w-16 h-8 rounded-xl font-semibold  hover:bg-green-600 hover:border border-black "
+                >Enviar</button>
+            </div>
+            
+
+        </div>
+
+    </div>
+</template>
+
